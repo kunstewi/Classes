e8b14d08808a01c934a04b48491335a90e12cff7
kunstewi <kan.ai35423@gmail.com>
2025-01-13 23:28:36 +0530

    clutter removed

--- Code/Node.js/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const os = require('os'); // os module
-
-// console.log('Platform:', os.platform());
-// console.log('CPU architecture:', os.arch());
-// console.log('Total memory:', os.totalmem());
-// console.log('Free memory:', os.freemem());
